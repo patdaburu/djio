2b971fb1d2f9edab442050e53a31c6fbc1c23c5b
patdaburu <pat@daburu.net>
2018-02-10 10:11:24 -0600

    Changed GeometryType to IntFlag and cleaned up hashing module.

--- djio/hashing.py
+++ djio/hashing.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-.. currentmodule:: djio.geometry
+.. currentmodule:: djio.hashing
 .. moduleauthor:: Pat Daburu <pat@daburu.net>
 
 Need to know at a glance if two geometries are the same?
@@ -10,6 +10,7 @@ Need to know at a glance if two geometries are the same?
 
 import math
 from typing import Iterable, Tuple
+
 
 def int_to_bytes(i: int,
                  width: int,
@@ -20,14 +21,15 @@ def int_to_bytes(i: int,
 
     :param i: the integer
     :param width: the number of bytes in the array
+    :param unsigned: `True` if the sign of the `int` may be disregarded, otherwise `False`
     :param as_iterable: when `True` the function returns a list of byte-sized `int`s instead of a `bytearray`.
     :return: the byte array
     """
     # We'll start by working with the absolute value and sort out negatives later.
     _i = i if (unsigned or i >=0) else int(math.fabs(i))
-    # Create a list to hold the bite-sized integers as we create them.
+    # Create a list to hold the bite-sized integers (bsi) as we create them.
     bsis = [0] * width
-    # We'll use a mask that spans eight bits (1 byte) which we'll shift in order to get only the bits in a given
+    # We'll use a mask that spans eight bits (one [1] byte) which we'll shift in order to get only the bits in a given
     # byte's position.
     mask = 255
     # Let's get started...
@@ -81,10 +83,23 @@ def bytes_to_int(b: bytes):
     # Return the number we have (or its negative twin).
     return i if not neg else i * -1
 
+
 def djiohash_v1(geometry_type_code: int,
                 srid: int,
                 coordinates: Iterable[Tuple[float, float] or Tuple[float, float, float]],
                 precision: int = 4) -> bytearray:
+    """
+    This is the first version of the djio hashing algorithm.
+    :param geometry_type_code: an integer indicating the type of the geometry
+    :param srid: the numeric spatial reference ID
+    :param coordinates: a flattened, ordered iteration of coordinates in the geometry expressed as tuples
+    :param precision: the maximum precision (points behind decimal places) to consider in the supplied coordinates
+    :return: a hash value for the geometry
+
+    .. sealso::
+
+        :py:class:`djio.geometry.GeometryType`
+    """
     # The first byte will hold the geometry type in the highest 4 bits of the first byte.
     # ☐☐☐☐0000
     b1_geometry_type = geometry_type_code << 4
@@ -103,19 +118,15 @@ def djiohash_v1(geometry_type_code: int,
 
     # Bytes 5-7 contains the total number of vertices.  However, we won't know it until we complete the iteration.
 
-    # Bytes 8-128 will contain the coordinate hashes.
-    b8_128 = [0] * 24 #TODO: 120 is a magic constant!!!
-
-    max_bits = 64
-    mask = int(math.pow(2, max_bits)) - 1 # A 32-bit mask
+    max_bits = 64  # the maximum number of bits in the coordinate hash TODO: This is a magic constant.
+    mask = int(math.pow(2, max_bits)) - 1  # a mask that covers all the bits
     coords_bits = 0  # This is the value we're doing all this bit blasting against.
-
+    # Let's go!
     coordinates_count = 0  # We'll keep the count as we iterate.
-    offset = 0
+    offset = 0  # With each iteration we'll shift to the left by this offset, then increment it.
     for coord in coordinates:
         for ord in coord:
             # Pull everything from the fractional part of the floating-point number into the whole part.
-
             ordi = int(ord * math.pow(10, precision)) # if ord < 0 else ~(int(ord * math.pow(10, precision)))
             # Rotate the integer value by the offset:
             # Shift the number bitwise by the offset.  (Some of the bits will move beyond the high-order part of
@@ -134,67 +145,20 @@ def djiohash_v1(geometry_type_code: int,
 
     coords_bits = ~(coords_bits & mask)
 
-    # Now we convert the bits to a series of byte-sized ints.
+    # Now we convert the bits to a series of byte-sized ints (bsi).
     coords_bsis = int_to_bytes(coords_bits, width=int(max_bits/8), unsigned=True, as_iterable=True)
 
+    # We can now establish the contents of bytes 5-7 since we know the total number of verticies.
     b5_7 = int_to_bytes(coordinates_count, width=3, as_iterable=True) # TODO: We could cut this down to 2 if we forbid vertex counts over 65536 or if we allow collisions here.
 
+    # Put all the byte-sized ints together into one big list.
     all_bsis = [b1] + b2_4 + b5_7 + coords_bsis
 
     # Convert our accumulated list of byte-sized ints into a single byte array.
     bytes = bytearray(all_bsis)
+    # Whew.
     return bytearray(bytes)
 
-
-
-
-# def djiohash_v1(geometry_type_code: int,
-#                 srid: int,
-#                 coordinates: Iterable[Tuple[float, float] or Tuple[float, float, float]],
-#                 precision: int = 4) -> bytearray:
-#     # The first byte will hold the geometry type in the highest 4 bits of the first byte.
-#     # ☐☐☐☐0000
-#     b1_geometry_type = geometry_type_code << 4
-#     # The next bit tells us whether or not the geometry is a collection.
-#     # 0000☐000
-#     b1_is_collection = 0 << 3  # TODO: Revisit this when collections are implemented.
-#     # The next bit tells us whether or not this geometry has 3D coordinates (i.e. "M" values).
-#     # 00000☐00
-#     b1_has_m_values = 0 << 2  # TODO: Revisit this when we can determine this.
-#     # The last two bits in this byte are presently available.
-#     # 000000☐☐ <-- These are available.
-#     b1 = b1_geometry_type | b1_is_collection | b1_has_m_values
-#
-#     # Bytes 2-4 contain the SRID.
-#     b2_4 = int_to_bytes(srid, width=3, as_iterable=True)
-#
-#     # Bytes 5-7 contains the total number of vertices.  However, we won't know it until we complete the iteration.
-#
-#     # Bytes 8-128 will contain the coordinate hashes.
-#     b8_128 = [0] * 24 #TODO: 120 is a magic constant!!!
-#
-#     how_many_bytes_per_coord = 12  # TODO: Rename this!!! And move the constant!
-#
-#     coordinates_count = 0  # We'll keep the count as we iterate.
-#     offset = 0
-#     for coord in coordinates:
-#         coord_bsis = _coord_to_bytes(coord, width=how_many_bytes_per_coord, precision=precision, as_iterable=True)
-#         for coord_bsi in coord_bsis:
-#             for i in range(0, how_many_bytes_per_coord):
-#                 bsi = coord_bsis[i]
-#                 if bsi != 0:
-#                     b8_128[i+offset] ^= bsi
-#         # Adjust the offset.
-#         offset += how_many_bytes_per_coord
-#         # Increment the coordinate count (we'll need it in a moment).
-#         coordinates_count += 1
-#
-#     b5_7 = int_to_bytes(coordinates_count, width=3, as_iterable=True) # TODO: We could cut this down to 2 if we forbid vertex counts over 65536 or if we allow collisions here.
-#
-#     lst = [b1] + b2_4 + b5_7 + b8_128
-#
-#     bytes = bytearray(lst)
-#     return bytearray(bytes)
 
 
 
